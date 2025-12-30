4e9b168c355c93883e9ae0f99949635b734df3c0
HuongVT <134629397+HuongVT0503@users.noreply.github.com>
2025-12-30 23:54:44 +0700

    fix http https discrepancy

--- FE/social-media/src/utils/formatDate.js
+++
@@ -1,8 +0,0 @@
-//convert dates to standard format ie 2hrs ago
-// export function formatDate(date) {
-//     const d = new Date(date);
-//     const month = d.getMonth() + 1;
-//     const day = d.getDate();
-//     const year = d.getFullYear();
-//     return `${month}/${day}/${year}`;
-// }
