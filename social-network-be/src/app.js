c1eb8b3987e6f336a1e77588c0cd1bc4ce87fabe
HuongVT <134629397+HuongVT0503@users.noreply.github.com>
2025-12-30 21:34:31 +0700

    fixxx url

--- social-network-be/src/app.js
+++ social-network-be/src/app.js
@@ -27,7 +27,9 @@ const app = express();
 
 // Middleware
 app.use(cors({
-  origin: process.env.FRONTEND_URL || 'http://localhost:5173',
+  origin: ['http://localhost:5173',
+    'https://group12-it-4409.vercel.app',
+  ],
   credentials: true,
 }));
 app.use(cookieParser());
